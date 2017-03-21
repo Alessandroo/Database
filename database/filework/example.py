557482d0f46155fa79ca0695ba5fc608cf20c7aa
DESKTOP-EU7S98I\Vojts <vojtseh2014@gmail.com>
2017-03-22 00:48:50 +0300

    Read big files

--- database/filework/example.py
+++ database/filework/example.py
@@ -7,8 +7,9 @@ class Ment:
         self.age = age
         self.language = ["greece", "spanish", "english"]
 
+
 if __name__ == '__main__':
-    filename = "ex.json"
+    filename = "ex1.json"
     print(Ment("lol", 18).__dict__, Ment("alala", 547).__dict__, Ment("helga").__dict__)
     a = to_json(Ment("lol", 18).__dict__)
     b = to_json(Ment("alala", 547).__dict__)
@@ -18,6 +19,9 @@ if __name__ == '__main__':
 
     print(text)
 
-    with open(filename, 'w+') as file:
-        for index in text:
-            file.write('{0}\n'.format(index))
+    with open(filename, 'w+') as file:  # a - дописать w+ писать с начала
+        for i in range(10):
+            for index in text:
+                print("begin: " + str(file.tell()))
+                file.write('{0}\n'.format(index))
+                print("end: " + str(file.tell()))
--- database/filework/reader.py
+++ database/filework/reader.py
@@ -5,8 +5,14 @@ if __name__ == '__main__':
     list = []
     try:
         with open(filename) as file:
-            for line in file.readlines():
-                list.append(from_json(line))
+            file.seek(151)
+            text = file.readline()
+            print(from_json(text))
+            # line = file[15]
+            # a = file.read
+            # print(a)
+            # for line in file.readlines():
+            #     list.append(from_json(line))
     except FileNotFoundError:
         print('File not found')
 
